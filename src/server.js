27b6bbf52f68ae825b4e32ea4788af93ac57d031
munyika christian <christianluanga@gmail.com>
2021-10-06 15:55:11 +0200

    final version

--- src/server.js
+++ src/server.js
@@ -13,8 +13,6 @@ app.use(morgan('dev'))
 app.post('/determine/hand', (req, res)=>{
     res.json(determine_hand(req.body.hand))
 })
-const {winningHand, rank} = classify_hand(hands.FIVE_OF_A_KIND, hands.FIVE_OF_A_KIND)
-console.log(winningHand)
 app.post('/classify/hand', (req, res)=>{
     const {hand1, hand2} = req.body
     res.json(classify_hand(hand1, hand2))
